aa2d502adcd51b330fffe68e47574ccf2096f2d7
Ben Ivey <benjaminivey00@gmail.com>
2020-12-27 14:46:54 -0700

    server file

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const app = express(); 
+
+
+let PORT = process.env.PORT || 8000;
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json()); 
+app.use( express.static('public'));
+
+
+
+
+
+
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT", PORT );
+})
